eff411bb6d01ed131a046d99408760cc91a9d916
Harsh Kumar Chaudhary <harsh.chaudhary@emporiumtech.com>
2013-11-20 09:07:32 +0530

    Added class for Make/Model/Year

---
+++ Src/Mojio/MMY.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mojio
+{
+    public class MMY  : StringEntity
+    {
+        public Guid? OwnerId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+    }
+}
