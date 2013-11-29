6deea32a2982535a7389b1cd2dc266cb1f8e0178
Narayan Sainaney <narayan@sainaney.com>
2013-11-28 18:58:50 -0800

    Create VehicleType Model

---
+++ Src/Mojio/VehicleType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mojio
+{
+    public class VehicleType : StringEntity
+    {     
+        public string Make
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+
+        public VehicleModelYear[] Models { get; set; }
+    }
+
+    public class VehicleModelYear
+    {
+        public string Model { get; set; }
+        public int[] Year { get; set; }
+    }
+}
