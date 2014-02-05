64c60fbb5fde70233c6cc1634575d45dbbf8c28f
Robb Lovell <robbl@moj.io>
2014-02-05 15:41:14 -0800

    took out reference counter from the client library.

--- Src/Mojio/Subscription.cs
+++ Src/Mojio/Subscription.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace Mojio
 {
@@ -26,44 +25,14 @@ namespace Mojio
         SignalR
     }
 
-    interface IReferenceCounter
-    {
-        void IncrementCount ();
-
-        void DecrementCount ();
-    }
-
-    public partial class Subscription : IReferenceCounter
-    {
-        private int _counter = 0;
-
-        public int Counter {
-            get { return _counter; } 
-            set {
-                _counter = value;
-                if (_counter < 0)
-                    _counter = 0;
-            }
-        }
-
-        public void IncrementCount ()
-        {
-            Counter += 1;
-        }
-
-        public void DecrementCount ()
-        {
-            Counter -= 1;
-        }
-    }
-
-    [JsonConverter(typeof(SubscriptionConverter))]
+    [JsonConverter (typeof(SubscriptionConverter))]
     public partial class Subscription : GuidEntity, IOwner
     {
-        public Subscription() {
+        public Subscription ()
+        {
         }
 
-        public Subscription(EventType type)
+        public Subscription (EventType type)
         {
             Event = type;
         }
@@ -87,13 +56,14 @@ namespace Mojio
         public DateTime? LastMessage { get; set; }
     }
 
-    [CollectionNameAttribute(typeof(Subscription))]
+    [CollectionNameAttribute (typeof(Subscription))]
     public partial class HardSubscription : Subscription
     {
-        public HardSubscription() {
+        public HardSubscription ()
+        {
         }
 
-        public HardSubscription(EventType type, float maxForce = 1f) : base(type)
+        public HardSubscription (EventType type, float maxForce = 1f) : base (type)
         {
             MaxForce = maxForce;
         }
@@ -101,13 +71,14 @@ namespace Mojio
         public float MaxForce { get; set; }
     }
 
-    [CollectionNameAttribute(typeof(Subscription))]
+    [CollectionNameAttribute (typeof(Subscription))]
     public partial class SpeedSubscription : Subscription
     {
-        public SpeedSubscription() {
+        public SpeedSubscription ()
+        {
         }
 
-        public SpeedSubscription(float maxSpeed = 65f, int interval = 60) : base(EventType.Speed)
+        public SpeedSubscription (float maxSpeed = 65f, int interval = 60) : base (EventType.Speed)
         {
             MaxSpeed = maxSpeed;
             Interval = 60;
