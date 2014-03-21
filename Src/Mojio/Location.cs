64d3a7a0e0124cbd92710d66a39de1c2faadb428
conneryn <conneryn@moj.io>
2014-03-21 13:26:57 -0700

    Fix Location IsValid check.  Move to private.

--- Src/Mojio/Location.cs
+++ Src/Mojio/Location.cs
@@ -9,7 +9,7 @@ namespace Mojio
     /// <summary>
     /// Location
     /// </summary>
-    public class Location : ICloneable
+    public partial class Location : ICloneable
     {
         /// <summary>
         /// latitude coordinate
@@ -32,29 +32,6 @@ namespace Mojio
         public double Dilution { get; set; }
 
         /// <summary>
-        /// Geospatial coordinates. Used by MongoDB.
-        /// Axis order: Longitude, Latitude
-        /// </summary>
-        [JsonIgnore]
-        public double[] Coordinates
-        {
-            get
-            {
-                if (!IsValid)
-                {
-                    return null;
-                }
-
-                return new double[] { Lng, Lat };
-            }
-            set
-            {
-                Lng = value[0];
-                Lat = value[1];
-            }
-        }
-
-        /// <summary>
         /// Gets a value indicating whether [is valid].
         /// </summary>
         /// <value><c>true</c> if [is valid]; otherwise, <c>false</c>.</value>
@@ -62,8 +39,7 @@ namespace Mojio
         {
             get
             {
-                // f#@$!ing floating numbers.
-                return !(Lat == double.NaN || Lng == double.NaN);
+                return !(double.IsNaN(Lat) || double.IsNaN(Lng));
             }
         }
 
@@ -74,22 +50,6 @@ namespace Mojio
         {
             Lat = double.NaN;
             Lng = double.NaN;
-        }
-
-        /// <summary>
-        /// Returns a <see cref="System.String" /> that represents this instance.
-        /// </summary>
-        /// <returns>A <see cref="System.String" /> that represents this instance.</returns>
-        public override string ToString()
-        {
-            return string.Format("Lat: {0}, Lng: {1}", Lat, Lng);
-        }
-        
-        /// <summary>Creates a new object that is a copy of the current instance.</summary>
-        /// <returns>A new object that is a copy of this instance.</returns>
-        public object Clone()
-        {
-            return this.MemberwiseClone();
         }
     }
 }
