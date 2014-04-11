074611fc5d895f03277593abfbc5cb6392d2afdc
Robb Lovell <robbl@moj.io>
2014-04-10 17:11:11 -0700

    Connection Lost and Fuel Event Classes

---
+++ Src/Mojio/Events/FuelEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mojio.Events
+{
+    class FuelEvent
+    {
+    }
+}
+namespace Mojio.Events
+{
+    /// <summary>
+    /// Fuel Event
+    /// </summary>
+    [CollectionNameAttribute(typeof(Event))]
+    public class FuelEvent : TripEvent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FuelEvent"/> class.
+        /// </summary>
+        public FuelEvent()
+        {
+            EventType = Events.EventType.LowFuel;
+        }
+    }
+}
+
