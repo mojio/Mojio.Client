1b7c514a1b4007ffd4d7031e09c7deabb9cce09f
Connery Noble <conneryn@moj.io>
2014-05-01 18:52:19 -0700

    Add Access and AccessRole to Mojio project.

---
+++ Src/Mojio/Access.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mojio
+{
+    public class Access : GuidEntity
+    {
+        public static Guid Everyone = Guid.Empty;
+
+        public Access()
+        {
+            UserId = Everyone;
+        }
+
+        public Guid UserId { get; set; }
+
+        public Guid RoleId { get; set; }
+
+        public string EntityType { get; set; }
+
+        public Guid EntityId { get; set; }
+    }
+}
---
+++ Src/Mojio/AccessRole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mojio
+{
+    [Flags]
+    public enum Permissions {
+        None = 0,
+        View = 1 << 0,
+        Share = 1 << 1,
+        Delete = 1 << 2,
+        Modify = 1 << 3
+    }
+
+    public class AccessRole : GuidEntity
+    {
+        public static Guid DefaultRolesAppId = Guid.Empty;
+
+        public AccessRole()
+        {
+            AppId = DefaultRolesAppId;
+        }
+
+        public string Name { get; set; }
+
+        public Guid AppId { get; set; }
+
+        public Permissions Permissions { get; set; }
+    }
+}
