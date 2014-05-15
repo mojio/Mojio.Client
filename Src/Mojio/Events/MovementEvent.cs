82f20832136973828d8e24ba1c335391bfb7acef
Leo Liang <ogirrk@gmail.com>
2014-05-14 17:08:01 -0700

    Created RPM and Movement events.

---
+++ Src/Mojio/Events/MovementEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mojio.Events
+{
+    /// <summary>
+    /// Movement Event
+    /// </summary>
+    [CollectionNameAttribute(typeof(Event))]
+    public class MovementEvent : TripEvent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MovementEvent"/> class.
+        /// </summary>
+        public MovementEvent()
+        {
+            EventType = Events.EventType.MovementStop;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MovementEvent"/> class.
+        /// </summary>
+        /// <param name="isStart">if set to <c>true</c> [is on].</param>
+        public MovementEvent(bool isStart)
+        {
+            EventType = isStart ? EventType.MovementStart : EventType.MovementStop;
+        }
+    }
+}
---
+++ Src/Mojio/Events/RPMEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mojio.Events
+{
+    /// <summary>
+    /// RPM Event
+    /// </summary>
+    [CollectionNameAttribute(typeof(Event))]
+    public class RPMEvent : TripEvent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RPMEvent"/> class.
+        /// </summary>
+        public RPMEvent()
+        {
+            EventType = Events.EventType.RPM;
+        }
+    }
+}
