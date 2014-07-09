df0cfaeaa4d463bfc3126baaabbb73f7d0b84d3a
Gerald Leung <geraldl@moj.io>
2014-07-09 16:17:27 -0700

    cleanup to device config stuff

--- Src/Mojio/EntityType.cs
+++ Src/Mojio/EntityType.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -40,6 +40,7 @@ namespace Mojio
         RefreshToken,
         Address,
         Location,
-		SimCard
+		SimCard,
+        MojioConfiguration
     }
 }
--- Src/Mojio/Serialization/TypeEnumDiscriminatorMap.cs
+++ Src/Mojio/Serialization/TypeEnumDiscriminatorMap.cs
@@ -1,4 +1,5 @@
-﻿using Mojio.Events;
+using Mojio.Events;
+using Mojio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
