e7fe165e36978296bea2f52296be3db51fc6dca5
Robb Lovell <robbl@moj.io>
2014-08-12 19:03:56 -0700

    MojioReport for the future mojio machine that produces it.

---
+++ Src/Mojio/MojioReport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mojio
+{
+    //TODO:: this could be produced by mojio machine as a log of the TCU report.
+    class MojioReport : GuidEntity, IOwner
+    {
+        public override EntityType Type
+        {
+            get { return EntityType.Mojio; }
+        }
+
+        /// <summary>
+        /// Owner ID
+        /// </summary>
+        [Observable(typeof(User))]
+        [Parent(typeof(User))]
+        [Obsolete("Use the access record to determine owner")]
+        public Guid? OwnerId { get; set; }
+    }
+}
