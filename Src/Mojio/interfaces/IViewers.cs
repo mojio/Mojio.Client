34be953c8146d2dd579246ad80ffc0659371c4bd
Robb Lovell <robbl@moj.io>
2014-08-22 16:30:16 -0700

    moved interfaces to folder.

---
+++ Src/Mojio/interfaces/IViewers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mojio
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IViewers
+    {
+        /// <summary>
+        /// list of viewers
+        /// </summary>
+        Guid[] Viewers { get; set; }
+    }
+}
