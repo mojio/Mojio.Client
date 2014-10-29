e19ad359979fbe11d2cf2f1b1fe88942652a61a1
Robb Lovell <robbl@moj.io>
2014-10-29 15:28:47 -0700

    General Conditional observer

--- Src/Mojio/Observers/ConditionalObserver.cs
+++ Src/Mojio/Observers/ConditionalObserver.cs
@@ -5,7 +5,42 @@ using System.Text;
 
 namespace Mojio.Observers
 {
-    class ConditionalObserver
+    [CollectionNameAttribute("Observer")]
+    class ConditionalObserver: ConditionalObserverBase
     {
+		/// <summary>
+		/// First threshold used in the conditional, required.
+		/// </summary>
+        public double Threshold1 { get; set; }
+
+        /// <summary>
+        /// Second threshold used in the conditional, optional.
+        /// </summary>
+        public double Threshold2 { get; set; }
+
+        /// <summary>
+        /// Second threshold used in the conditional, optional.
+        /// </summary>
+        public double Threshold2 { get; set; }
+
+		/// <summary>
+		/// When true, this is an exit fence, if false, this is an entry fence.
+        /// If null, this is both an exit and entry fence
+		/// </summary>
+        public bool? ConditionValue { get; set; }
+
+		public ConditionalObserver()
+            : base(ObserverType.GeoFence, typeof(Vehicle))
+        {
+
+        }
+
+        public ConditionalObserver(Guid vehicleId, Location location, double radiusInKm)
+            : this()
+        {
+            SubjectId = vehicleId;
+            Location = location;
+            Radius = radiusInKm;
+        }
     }
 }
