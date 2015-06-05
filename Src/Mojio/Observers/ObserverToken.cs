231e20ce7da40aadce35e530635aec3b8883f33e
tasha420 <tasha42092@hotmail.com>
2015-06-05 11:01:32 -0700

    Added Basic Auth to Http Post observer

--- Src/Mojio/Observers/ObserverToken.cs
+++ Src/Mojio/Observers/ObserverToken.cs
@@ -81,6 +81,10 @@ namespace Mojio
     {
         public string Address { get; set; }
 
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
         public HttpPostObserverToken()
         {
             Transport = Transport.HttpPost;
